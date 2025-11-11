f82ee064a47619150283531f72049552c3beb435
aahmedhusseinn <ahmedmsa23.5@gmail.com>
2025-11-11 09:44:26 +0200

    Implement Instructor class inheriting from Person

--- Instructor.h
+++ Instructor.h
@@ -3,17 +3,22 @@
 
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
+class Instructor:public Person {
+    private:
+    string department;
+    int experienceYears;
 
+    public:
+    Instructor();
+    Instructor(string name, int id, string department, int experienceYears);
+    ~Instructor();
 
+    void setDepartment(string department);
+    void setExperienceYears(int years);
 
+    string getDepartment();
+    int getExperienceYears();
 
-
-
-
-
-
-
-
-
+    void display() const override;
+};
 #endif
