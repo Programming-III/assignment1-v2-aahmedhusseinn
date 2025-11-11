2f1a5fac5295f58854b9977e6588020492eb0153
aahmedhusseinn <ahmedmsa23.5@gmail.com>
2025-11-11 09:25:33 +0200

    Define Person class with constructors and methods

--- Person.h
+++ Person.h
@@ -4,17 +4,22 @@
 #include <string>
 using namespace std;
 
-//#write Person class here
+class Person {
+private:
+string name;
+int id;
 
+public:
+    Person();
+    Person(string name, int id);
+    virtual ~Person();
 
+    void setName(string name);
+    void setId(int id);
 
+    string getName() const;
+    int getId() const;
 
-
-
-
-
-
-
-
-
+    virtual void display() const;
+};
 #endif
